f41492667d319e630d3467d44e3e7e07a0b58e60
swapnilNJ <swapnil.nuron@gmail.com>
2022-09-24 22:45:04 +0530

    please find code

---
+++ src/Removespace.java
@@ -0,0 +1,24 @@
+
+public class Removespace {
+public static void main(String[]args)
+{
+	RemoveSpace();
+	spaceinletter();
+}
+public static void RemoveSpace()
+{
+	
+	String s=" sangita jori";
+	{
+		System.out.println(s.replace(" ", ""));
+	}	
+}
+
+public static void spaceinletter()
+{
+	String s1="sangita swapnil Najan";
+	{
+		System.out.println(s1.replace("", " "));
+	}
+}
+}
